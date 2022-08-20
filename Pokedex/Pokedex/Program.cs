c22daf62ac7f8ea880eea7f5b2b114018e056b0b
João Lessa <joaovictorlessa98@gmail.com>
2022-08-19 23:02:33 -0300

    start

---
+++ Pokedex/Pokedex/Program.cs
@@ -0,0 +1,30 @@
+﻿using RestSharp;
+namespace Pokedex
+{
+    class Program
+    {
+        static void Main (string [] args)
+        {
+            InvocarGet();
+        }
+
+        private static void InvocarGet()
+        {
+            string pokemon = "bulbasaur";
+            var client = new RestClient($"https://pokeapi.co/api/v2/pokemon/{pokemon}");
+            
+            RestRequest request = new RestRequest ("", Method.Get);
+            var response = client.Execute(request);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                Console.WriteLine (response.Content);
+            }
+            else
+            {
+                Console.WriteLine(response.ErrorMessage);
+            }
+        }
+    }
+}
+
